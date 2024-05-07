259024bd29bb1f49fbdb8027273cfb217a2e0169
Tamim Hossain <77679582+stamim0@users.noreply.github.com>
2024-05-07 18:22:58 +0600

    transaction table create

---
+++ database/migrations/2024_05_07_114002_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->on('users')->onDelete('cascade');
+            $table->string('transaction_type');
+            $table->double('amount')->nullable();
+            $table->double('fee')->nullable();
+            $table->date('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
